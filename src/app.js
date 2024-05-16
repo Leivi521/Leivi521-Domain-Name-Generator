d9836532a87a7575668d9d48904165a91e8a6ecd
Leivi521 <165715355+Leivi521@users.noreply.github.com>
2024-05-16 02:57:44 +0000

    first save

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
+};
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function arraySelector(array) {
+  let randomelement = array[Math.floor(Math.random() * array.length)];
+  return randomelement;
+}
+function domainGenerator() {
+  let domain =
+    arraySelector(pronoun) +
+    " " +
+    arraySelector(adj) +
+    " " +
+    arraySelector(noun) +
+    " ";
+  return domain;
+
+document.getElementById(domain).textContent = domainGenerator();
 };
